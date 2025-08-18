373bfb35ab204f1339c71ec5b04dffb33582c6b5
yj99yj <yongyongjoo@gmail.com>
2025-08-18 14:12:51 -0400

    notification

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 import 'reactjs-popup/dist/index.css';
 import BookingConsultation from './Components/BookingConsultation'
+import Notification from "./Components/Notification/Notification";
 
 function App() {
   return (
@@ -15,12 +16,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-
-
-          <Route path="/signup" element={<Sign_Up />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Sign_Up />} />
           <Route path="/instant-consultation" element={<InstantConsultation />} />
           <Route path="/Bookingconsultation" element={<BookingConsultation />} />
+          <Route path="<Notification>" element={<Notification/>}/> 
         </Routes>
       </BrowserRouter>
     </div>
